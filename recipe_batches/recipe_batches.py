a6c7dc18f18cb45d4b9a07669acd3d730482501f
Addison Hill <adddison.hill@gmail.com>
2020-03-18 19:10:38 -0500

    finished stock prices function to return max profit, working on recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,24 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    total_batches = 0
+    if len(recipe) == len(ingredients):
+        for i in recipe:
+            print(ingredients[i])
+            if recipe[i] <= ingredients[i]:
+                total_batches += 1
+                ingredients[i] = ingredients[i] - recipe[i]
+                print(ingredients[i])
+
+    return total_batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,30 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    # make variables for current_min_price_so_far and max_profit_so_far
+    cur_min = 0
+    max_profit = [] # <----- we make this an array of all the profits and then find largest one. Thanks Ethan from help channel lol
+
+    # To fill the max profit, we now need to set the cur_min and compare to each element in the array. Finding each profit.
+    for i in range(0, len(prices) - 1):
+        cur_min = prices[i]
+
+        for j in range(i+1, len(prices) -1):
+            max_profit.append(prices[j] - cur_min) # <---- This will give us all the potential profits into one list
+    
+    return max(max_profit)
+
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
